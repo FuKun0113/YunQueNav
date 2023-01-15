ad1c73fe180c5e86787b31fcf0707ebd97a9d130
FuKun0113 <a2435454985@gmail.com>
2023-01-15 13:29:27 +0800

    项目重构

---
+++ Client/src/pages/Page404.js
@@ -0,0 +1,51 @@
+import { Helmet } from 'react-helmet-async';
+import { Link as RouterLink } from 'react-router-dom';
+// @mui
+import { styled } from '@mui/material/styles';
+import { Button, Typography, Container, Box } from '@mui/material';
+
+// ----------------------------------------------------------------------
+
+const StyledContent = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  padding: theme.spacing(12, 0),
+}));
+
+// ----------------------------------------------------------------------
+
+export default function Page404() {
+  return (
+    <>
+      <Helmet>
+        <title> 404 Page Not Found | Minimal UI </title>
+      </Helmet>
+
+      <Container>
+        <StyledContent sx={{ textAlign: 'center', alignItems: 'center' }}>
+          <Typography variant="h3" paragraph>
+            抱歉，该页面不存在！
+          </Typography>
+
+          <Typography sx={{ color: 'text.secondary' }}>
+            非常抱歉，我们无法找到您正在寻找的页面。也许你输错了网址？请检查您的输入是否有误。
+          </Typography>
+
+          <Box
+            component="img"
+            src="/assets/illustrations/illustration_404.svg"
+            sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
+          />
+
+          <Button to="/" size="large" variant="contained" component={RouterLink}>
+            回到主页
+          </Button>
+        </StyledContent>
+      </Container>
+    </>
+  );
+}
